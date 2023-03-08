aa2395234505f92cfc83fafc6d9a785b2032f4d2
Isma_b3lu <zorikdima20010@gmail.com>
2023-03-08 16:52:47 +0400

    2

---
+++ main.py
@@ -0,0 +1,13 @@
+class Animals():
+    year = 5
+    height = 30
+    class cat():
+        year  = 3
+        height = 50
+        weight = 26
+        class dog():
+            year = 7
+            height = 40
+            weight = 20
+
+
